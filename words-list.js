92f45d362e2ecb5a996a148272e4a4a8ab43e97f
dschneideramphi <dschneider@amphi.com>
2019-10-01 08:23:14 -0700

    added image stuff

--- words-list.js
+++ words-list.js
@@ -23,3 +23,50 @@ async function getAPI(word) {
     containerElt.appendChild(newDiv);
   }
 }
+
+
+let key = "120cf60e930e396adc145a3e7d3d9ea688a3d501856112432d09981a40889a39";
+let images;
+
+//SEARCH QUERY STRING - WORKS!
+async function searchImage(search) {
+  images = await fetch(`https://api.unsplash.com/search/photos/?query=${search}&orientation=squarish&client_id=${key}`)
+    .then(res =>  res.json())
+        .then((result) => {
+          console.log(result);
+          return result.results;
+        }
+    );
+
+    containerElt.innerHTML = "";
+    for(img of images) {
+      let newImg = document.createElement("img");
+      newImg.style.height = "500px";
+      newImg.style.width = "500px";
+      newImg.src = img.urls.small;
+      containerElt.appendChild(newImg);
+    }
+
+  }
+
+
+//GETS RANDOM IMAGES!
+async function randomImage(amt) {
+  images = await fetch(`https://api.unsplash.com/photos/random/?count=${amt}&client_id=${key}`)
+    .then(res =>  res.json())
+        .then((result) => {
+          console.log(result);
+          return result;
+        }
+    );
+
+    containerElt.innerHTML = "";
+    for(img of images) {
+      let newImg = document.createElement("img");
+      newImg.style.height = "500px";
+      newImg.style.width = "500px";
+      newImg.src = img.urls.small;
+      containerElt.appendChild(newImg);
+    }
+
+  }
